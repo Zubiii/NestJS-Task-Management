590a8d680398ab8b58445575d924cd269804eeab
zubiii <zubair.javed.ch@gmail.com>
2022-10-29 07:35:46 -0500

    add transform interceptor in order to not show user details

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransfomInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransfomInterceptor());
   await app.listen(3000);
 }
 bootstrap();
