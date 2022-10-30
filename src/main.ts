c55d4a1dc0e02a762c38efbadab5cc48dc9fbe68
zubiii <zubair.javed.ch@gmail.com>
2022-10-29 23:10:21 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,23 @@ import { TransfomInterceptor } from './interceptor/transform.interceptor';
 
 const port = 3000;
 async function bootstrap() {
+  // Init logger
   const logger = new Logger('Application Started', { timestamp: true });
+
   const app = await NestFactory.create(AppModule);
+
+  // Set prefix to the route
   app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe());
+
+  // Set Global Interceptor
   app.useGlobalInterceptors(new TransfomInterceptor());
+
+  // Enable CORS
+  app.enableCors();
+
+  //litining on port
   await app.listen(port);
   logger.log(`Application is running on Port ==> ${port}`);
 }
