3a1bd21c8757d05ff1a128bd81ca110267e0e602
zubiii <zubair.javed.ch@gmail.com>
2022-12-03 09:47:46 -0600

    add shadow to the welcome banner

--- client/src/views/Profile/ProfileIndex.vue
+++ client/src/views/Profile/ProfileIndex.vue
@@ -2,7 +2,7 @@
   <div class="bg-stone h-screen">
     <header-layout></header-layout>
     <div class="flex flex-col">
-      <div class="m-10 bg-orange400/30 p-10 w-100 rounded-lg text-left">
+      <div class="shadow m-10 bg-orange400/30 p-10 w-100 rounded-lg text-left">
         <h1 class="uppercase font-bold">Hello {{ username }}!</h1>
         <p class="font-medium">
           Welcome back to Task Managment Board
