4bf53de499ece432bb6e838da48a1733dea61628
zubiii <zubair.javed.ch@gmail.com>
2022-12-04 19:18:33 -0600

    added vue3-smooth-dnd

---
+++ client/src/components/KanbanItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <Draggable v-if="item">
+      <div class="cursor-move my-2 mx-4 rounded-lg shadow-md dark:bg-gray-800 hover:border-2 border-primary">
+        <div v-if="item.loading" class="flex space-x-2 rounded-lg bg-orange400/75 text-gray-900 dark:text-gray-200 dark:bg-gray-900 px-4 py-2 rounded-t-lg">
+          <span>Processing...</span>
+        </div>
+        <div class="p-4 space-y-2 bg-white rounded-lg">
+          <div class="rounded-lg bg-primary p-2 w-max h-max inline-block box-content  ">
+            {{emoji}}
+          </div>
+        <p>{{ item.data }}</p>
+        </div>
+      </div>
+    </Draggable>
+  </template>
+  
+  <script>
+  import { getRandomEmoji } from '../helpers/Vue3Smooth'
+  import { Draggable } from 'vue3-smooth-dnd'
+  export default {
+    name: "KanbanItem",
+    components: { Draggable },
+    data(){ 
+      return {
+        emoji: getRandomEmoji()
+      }
+    },
+    props: {
+      item: Object,
+    },
+  };
+  </script>
--- client/src/views/Profile/Table/IndexTable.vue
+++ client/src/views/Profile/Table/IndexTable.vue
@@ -1,14 +1,121 @@
 <template>
-  <h1>Vue Draggable</h1>
+  <Container
+    class="h-full flex overflow-x-auto gap-8 outline outline-orange400/20 outline-2 rounded-lg p-2"
+    group-name="cols"
+    tag="div"
+    orientation="horizontal">
+    <Draggable class=" bg-orange400/20 rounded-lg h-full w-96 flex-shrink-0 shadow-xl"
+      v-for="column in scene.children" :key="column.id">
+      <div class="h-full flex flex-col">
+
+        <!-- header-->
+        <div class="cursor-move rounded-t-lg p-4 space-x-4 bg-primary text-black flex space-x-2">
+          <span class="text-lg">{{ column.name }}</span>
+        </div>
+        <!-- column -->
+        <Container
+          class="flex-grow overflow-y-auto overflow-x-hidden bg-stone"
+          orientation="vertical"
+          group-name="col-items"
+          :shouldAcceptDrop="(e, payload) =>  (e.groupName === 'col-items' && !payload.loading)"
+          :get-child-payload="getCardPayload(column.id)"
+          :drop-placeholder="{ className: 
+            `bg-primary bg-opacity-20  
+            border-dotted border-2 
+            border-primary rounded-lg mx-4 my-2`, 
+          animationDuration: '200', 
+          showOnTop: true }"
+          drag-class="bg-primary dark:bg-primary 
+            border-2 border-primary-hover text-white 
+            transition duration-100 ease-in z-50
+            transform rotate-6 scale-110"
+          drop-class="transition duration-100 
+            ease-in z-50 transform 
+            -rotate-2 scale-90"
+          @drop="(e) => onCardDrop(column.id, e)">
+
+            <!-- Items -->
+            <KanbanItem v-for="item in column.children" :key="item.id" :item="item"></KanbanItem>
+        </Container>
+      </div>
+    </Draggable>
+  </Container>
 </template>
 
 <script>
-// const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-// Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-// Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-// const columnNames = ['OPEN', 'IN_PROGRESS', 'DONE']
-
+import { Container, Draggable } from 'vue3-smooth-dnd'
+import { applyDrag, generateItems, generateWords } from '../../../helpers/Vue3Smooth.js'
+import KanbanItem from '../../../components/KanbanItem.vue'
+// mock
+const scene = {
+  type: 'container',
+  props: {
+    orientation: 'horizontal'
+  },
+  children: generateItems(3, i => ({
+    id: `column${i}`,
+    type: 'container',
+    name: generateWords(Math.random() * 2 + 1),
+    props: {
+      orientation: 'vertical',
+    },
+    children: generateItems(+(Math.random() * 10).toFixed() + 5, j => ({
+      type: 'draggable',
+      id: `${i}${j}`,
+      loading: false,
+      data: generateWords(Math.random() * 12 + 2)
+    }))
+  }))
+}
 export default {
-
+  components: { Container, Draggable, KanbanItem},
+  data () {
+    return {
+      scene,
+    }
+  },
+  mounted(){},
+  methods: {
+    getColumnHeightPx(){
+      let kanban = document.getElementById('kanbanContainer');
+      return kanban ? kanban.offsetHeight - 122 : 0;
+    },
+    onCardDrop (columnId, dropResult) {
+      // check if element where ADDED or REMOVED in current collumn
+      if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+        
+        const scene = Object.assign({}, this.scene)
+        const column = scene.children.filter(p => p.id === columnId)[0]
+        const itemIndex = scene.children.indexOf(column)
+        const newColumn = Object.assign({}, column)
+        
+        // check if element was ADDED in current column
+        if((dropResult.removedIndex == null && dropResult.addedIndex >= 0)){
+          // your action / api call
+          dropResult.payload.loading = true
+          // simulate api call
+          setTimeout(function(){ dropResult.payload.loading = false }, (Math.random() * 5000) + 1000); 
+        }
+        
+        newColumn.children = applyDrag(newColumn.children, dropResult)
+        scene.children.splice(itemIndex, 1, newColumn)
+        this.scene = scene
+      }
+    },
+    getCardPayload (columnId) {
+      return index => {
+        return this.scene.children.filter(p => p.id === columnId)[0].children[index]
+      }
+    },
+  }
 }
 </script>
+<style>
+/** NB: dont remove, 
+* When using orientation="horizontal" it auto sets "display: table"
+* In this case we need flex and not display table  
+*/
+.smooth-dnd-container.horizontal{
+  display: flex !important;
+}
+</style>
