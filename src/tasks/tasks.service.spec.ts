6946a7e616fd7be4d5ce6db8982df0e0d8bc55b1
zubiii <zubair.javed.ch@gmail.com>
2022-12-27 16:17:05 -0600

    add test for tasks

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,43 +1,68 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { getRepositoryToken } from '@nestjs/typeorm';
-// import { AuthController } from 'src/auth/auth.controller';
-// import { AuthService } from 'src/auth/auth.service';
-// import { Task } from './task.entity';
-// import { TasksController } from './tasks.controller';
-// import { TasksService } from './tasks.service';
+import { Test } from '@nestjs/testing';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
-// const mockTask = {
-//   title: 'test title',
-//   description: 'test decription',
-//   status: 'OPEN',
-//   userId: 'testUserID',
-// };
+const mockTask = {
+  title: 'test title',
+  description: 'test decription',
+  status: 'OPEN',
+  userId: 'testUserID',
+};
+const mockUser = {
+  id: 'jest-id-9876364',
+  username: `jest-${Date.now().toString()}@test.com`,
+  password: 'P@$$w0rd',
+  tasks: [],
+};
 
-// describe('Tasks Controller', () => {
-//   let tasksController: TasksController;
-//   let tasksService: TasksService;
+const mockTaskService = {
+  createTask: jest.fn((dto) => {
+    return {
+      id: 'random-id-32',
+      ...dto,
+    };
+  }),
+  getAllTasks: jest.fn(() => {
+    return [];
+  }),
+};
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [TasksController],
-//       providers: [
-//         TasksService,
-//         { provide: getRepositoryToken(Task), useValue: mockTask },
-//       ],
-//     }).compile();
+describe('Tasks Controller is testing', () => {
+  let tasksController: TasksController;
+  let tasksService: TasksService;
 
-//     tasksService = module.get(TasksService);
-//     tasksController = module.get(TasksController);
-//   });
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [TasksController],
+      providers: [TasksService],
+    })
+      .overrideProvider(TasksService)
+      .useValue(mockTaskService)
+      .compile();
 
-//   describe('Get All Tasks', () => {
-//     it('should return array of Tasks', async () => {
-//       tasksService.getAllTasks(mockUser);
-//     });
-//   });
-// });
-describe('tasks', () => {
-  it('True Case', () => {
-    expect(4).toEqual(4);
+    tasksController = module.get(TasksController);
+    tasksService = module.get(TasksService);
+  });
+
+  it('Task Controller should be defined', () => {
+    expect(tasksController).toBeDefined();
+  });
+
+  it('create Task', () => {
+    expect(tasksService.createTask).not.toHaveBeenCalled();
+    expect(tasksController.createTask(mockTask, mockUser)).toEqual({
+      description: expect.any(String),
+      id: expect.any(String),
+      status: expect.any(String),
+      title: expect.any(String),
+      userId: expect.any(String),
+    });
+    expect(tasksService.createTask).toHaveBeenCalled();
+  });
+
+  it('Get All Tasks', () => {
+    expect(tasksService.getAllTasks).not.toHaveBeenCalled();
+    expect(tasksController.getAllTasks(mockUser)).toEqual([]);
+    expect(tasksService.getAllTasks).toHaveBeenCalled();
   });
 });
